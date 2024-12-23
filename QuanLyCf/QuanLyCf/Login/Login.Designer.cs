6fe475296ff81911560fd505b7e85b239cd27e03
Bùi Khắc Huy <buikhachuy003@gmail.com>
2024-12-23 16:13:58 +0700

    anh em clone về đi

--- QuanLyCf/QuanLyCf/Login/Login.Designer.cs
+++ QuanLyCf/QuanLyCf/Login/Login.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace QuanLyCf
+﻿namespace QuanLyCf.Login
 {
     partial class Login
     {
--- QuanLyCf/QuanLyCf/Login/Login.cs
+++ QuanLyCf/QuanLyCf/Login/Login.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace QuanLyCf
+namespace QuanLyCf.Login
 {
     public partial class Login : Form
     {
